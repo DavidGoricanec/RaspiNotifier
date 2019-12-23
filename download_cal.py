73d14810ed3118498a3eaa092c3ee61b2d6d62ee
David Goricanec <david.goricanec@edu.fh-joanneum.at>
2019-12-23 22:50:19 +0100

    Kalender download und Testdaten

---
+++ download_cal.py
@@ -0,0 +1,11 @@
+import urllib.request
+
+print('Downloading calender file...')
+
+# Contains download link of file
+url = 'https://elearning.fh-joanneum.at/calendar/export_execute.php?userid=2629&authtoken=beb91d35194fc3177ed3b57ae34608829bf15b09&preset_what=all&preset_time=monthnow'
+
+# Downloads file using download link as first parameter and destination as second paramter
+urllib.request.urlretrieve(url, '/home/pi/Dokumente/Scripting_Projekt/kalender.ics')
+
+print('finished')
